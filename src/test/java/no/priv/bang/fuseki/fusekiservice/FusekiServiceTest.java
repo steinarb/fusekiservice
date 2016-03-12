3dc04a1123a867b6b22d2bb70e62a5a120963c31
Steinar Bang <sb@dod.no>
2016-03-12 23:28:35 +0100

    Now uses log capture to get port number verification in unit test.
    The fuseki methods all have void return values, so there is no simple
    way to find something to assert on.
    
    Mocked a log4j appender and added it to the root logger, then captured
    the logs, found the log message containing the port number and verified
    that it had the expected port number.

--- src/test/java/no/priv/bang/fuseki/fusekiservice/FusekiServiceTest.java
+++ src/test/java/no/priv/bang/fuseki/fusekiservice/FusekiServiceTest.java
@@ -1,15 +1,47 @@
 package no.priv.bang.fuseki.fusekiservice;
 
+import static org.hamcrest.core.StringEndsWith.endsWith;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.util.List;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class FusekiServiceTest {
+
+    @Mock
+    private Appender mockAppender;
+
+    @Captor
+    private ArgumentCaptor<LoggingEvent> captorLoggingEvent;
+    private Logger rootLogger;
+
+    @Before
+    public void setup() {
+        when(mockAppender.getName()).thenReturn("MockAppender");
+        rootLogger = Logger.getRootLogger();
+    }
 
     @Test
     public void test() throws InterruptedException {
         FusekiService endpoint = new FusekiService();
+    	int linenumberOfLogLineWithPortNumber = 7;
+        rootLogger.addAppender(mockAppender);
         endpoint.startDaemon();
+        verify(mockAppender, times(linenumberOfLogLineWithPortNumber)).doAppend(captorLoggingEvent.capture());
+        List<LoggingEvent> allLogEventsSoFar = captorLoggingEvent.getAllValues();
+        assertThat(allLogEventsSoFar.get(linenumberOfLogLineWithPortNumber - 1).getRenderedMessage(), endsWith("port 4000"));
         Thread.sleep(10000);
         endpoint.stopDaemon();
     }
